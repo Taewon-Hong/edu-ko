36303cc9e786ccf4e0872539e063cc4245b29ab3
Taewon-Hong <ssakuy88@naver.com>
2021-03-10 09:56:17 +0900

    비밀정보보

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 7000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ssakuy88:ghdtjdrb88@edu.h4wfj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+const config = require('./config/key')
+
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err))
 
-
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요')
+  res.send('Hello World! 안녕하세요1234')
+})
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
